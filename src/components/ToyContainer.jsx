6a8c0df18faca66e89f48f9fb5c779878d0ced9f
Drew Keat <thedrewkeat@gmail.com>
2022-05-14 14:52:50 -0600

    npm install

--- src/components/ToyContainer.jsx
+++ src/components/ToyContainer.jsx
@@ -1,12 +1,10 @@
-import React from 'react';
-import ToyCard from './ToyCard'
+import React from "react";
+import ToyCard from "./ToyCard";
 
 const ToyContainer = () => {
-  return(
-    <div id="toy-collection">
-      {/* Render the collection of ToyCards */}
-    </div>
+  return (
+    <div id="toy-collection">{/* Render the collection of ToyCards */}</div>
   );
-}
+};
 
 export default ToyContainer;
